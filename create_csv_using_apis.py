0414afd793e009726c496352c99c9b3ed28e2738
youval <youval@gmail.com>
2025-07-27 18:53:29 -0700

    update

--- create_csv_using_apis.py
+++ create_csv_using_apis.py
@@ -3,7 +3,7 @@ from src.utils import read_trades_info, print_month_summary
 
 if __name__ == "__main__":
     # Change to the month to collect
-    dt = "2024-12"
+    dt = "2025-01"
     save_to_file = True
     df_result = read_trades_info(dt, save_to_file=save_to_file)
     print("\n----- Month Summary -----")
